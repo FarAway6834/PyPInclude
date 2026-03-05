939d0c348eff6c78ef3b26c06f05678a47b1d2bd
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2026-03-06 08:24:37 +0900

    setup.py 업데이트

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name="PyPInclude",
-    version="0.0.1",
+    version="0.0.2",
     description = "PyPInclude (PyPI includer)",
     py_modules=["PyPInclude"],
-    install_requires=["setuptools"],
+    install_requires=["setuptools", "privathon"],
     author='du7ec',
     author_email='dutec6834@gmail.com',
-    keywords=['PyPI', 'include', "PyPInclude", "PyPIncluder", "includer", "PyPIinclude", "PyPIInclude", "PyPIincluder", "PyPIIncluder", "setuptools", "include_files", "files", "pypi", "pypi_includer", "pypi_include"], url='https://github.com/FarAway6834/PyPInclude'
+    keywords=["python", 'PyPI', 'include', "PyPInclude", "PyPIncluder", "includer", "PyPIinclude", "PyPIInclude", "PyPIincluder", "PyPIIncluder", "setuptools", "include_files", "files", "pypi", "pypi_includer", "pypi_include"], url='https://github.com/FarAway6834/PyPInclude'
 )
