49ca2560cbb8fc674c1e826722bf8cabc2bf19da
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2026-03-05 18:03:22 +0900

    setup.py 업데이트

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="PyPInclude",
     version="0.0.1",
-    py_modules=["PyPInclude"],
+    py_modules=["PyPInclude, an PyPI includer"],
     install_requires=["setuptools"],
     author='du7ec',
     author_email='dutec6834@gmail.com',
