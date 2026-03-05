d784b4cbe4cb9f5de0f3575d16e15bb5d084ebd4
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2026-03-05 17:37:55 +0900

    PyPInclude.py 업데이트

--- PyPInclude.py
+++ PyPInclude.py
@@ -1,9 +1,14 @@
 from setuptools import setup as __setup__
+from setuptools.command.install import install as __install__
+from setuptools.command.develop import develop as __develop__
 from setuptools import find_packages
 from os import listdir as __ls__
 from os.path import join as __join_path__
 from os.path import isdir as __is_dir__
 from os.path import isfile as __is_file__
+from os.path import sep as __path_sep__
+from os.path import basename as __base_name__
+from os import system as __shell__
 
 def __return_name_if_is_exist_in__(f, target, where):
     ret = __join_path__(where, target)
@@ -17,7 +22,16 @@ def __core__(package_paths, **kargv):
             if "package_data" in ret: ret["package_data"][package_path] = ["__PyPInclude__/*"]
             else: ret["package_data"] = { package_path : ["__PyPInclude__/*"] }
             if __return_name_if_is_exist_in__(__is_file__, "__main__.py", PyPInclude):
-                if 
+                if "cmdclass" in ret: ret["cmclass"].append(__base_name__(package_path))
+                else: ret["cmclass"] = [__base_name__(package_path)]
+    if "cmclass" in ret:
+        cmclass = ret["cmclass"]
+        ret["cmclass"] = {}
+        class install(__install__):
+            def run(self):
+                __install__.run(self)
+                installed_dir = self.install_lib
+                for init_script in map(lambda x : __join_path__(installed_dir, x, "__PyPInclude__", "main.py"), filter(cmclass.__contains__, self.distribution.packages)): __shell__(f"python -m {init_script}")
     return ret
 
 def setup(*, package_paths, **kargv):
