4fe03af574b70ae1ff1c52a8e9e5ffb48e71d411
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2026-03-05 18:03:03 +0900

    setup.py 만들기

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="PyPInclude",
+    version="0.0.1",
+    py_modules=["PyPInclude"],
+    install_requires=["setuptools"],
+    author='du7ec',
+    author_email='dutec6834@gmail.com',
+)
